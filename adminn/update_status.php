bf8cf820fd4d9702bd882ecf4479ed9236ff6e06
dxAryo <gilang91668@gmail.com>
2023-07-05 18:58:51 +0700

    Update update_status.php

--- adminn/update_status.php
+++ adminn/update_status.php
@@ -512,6 +512,61 @@ if ($hasil->num_rows > 0) {
         });
     });
 </script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+  $(document).ready(function() {
+    $(".search").keyup(function() {
+      var searchTerm = $(this).val().toLowerCase();
+      $(".table tbody tr").each(function() {
+        var lineText = $(this).text().toLowerCase();
+        if (lineText.includes(searchTerm)) {
+          $(this).show();
+        } else {
+          $(this).hide();
+        }
+      });
+    });
+  });
+
+  $(document).ready(function() {
+    $(".search").keyup(function() {
+      var searchTerm = $(".search").val();
+      var listItem = $(".results tbody").children("tr");
+      var searchSplit = searchTerm.replace(/ /g, "'):containsi('");
+
+      $.extend($.expr[":"], {
+        containsi: function(elem, i, match, array) {
+          return (
+            (elem.textContent || elem.innerText || "")
+              .toLowerCase()
+              .indexOf((match[3] || "").toLowerCase()) >= 0
+          );
+        },
+      });
+
+      $(".results tbody tr")
+        .not(":containsi('" + searchSplit + "')")
+        .each(function(e) {
+          $(this).attr("visible", "false");
+        });
+
+      $(".results tbody tr:containsi('" + searchSplit + "')").each(function(e) {
+        $(this).attr("visible", "true");
+      });
+
+      var jobCount = $(".results tbody tr[visible='true']").length;
+      $(".counter").text(jobCount + " item");
+
+      if (jobCount == "0") {
+        $(".no-result").show();
+      } else {
+        $(".no-result").hide();
+      }
+    });
+  });
+</script>
 
   </body>
 </html>
