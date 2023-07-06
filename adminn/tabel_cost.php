718a6f95756cbbeffebe8ed5bb37843542cb9596
dxAryo <gilang91668@gmail.com>
2023-07-06 09:57:43 +0700

    Update tabel_cost.php

--- adminn/tabel_cost.php
+++ adminn/tabel_cost.php
@@ -1,80 +1,99 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Stock Quantities for Pewangi bulan</title>
+    <title>Jumlah Order per Bulan</title>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <style>
-        .tabel {
+        .container {
             width: 800px;
+            margin: 100px auto;
+        }
+        .tabel{
+            width: 700px;
             margin: auto;
         }
     </style>
 </head>
 <body>
     <div class="tabel">
-        <canvas id="tiga"></canvas>  
-        <?php
-        include '../koneksi.php';   
-        // Query to retrieve data
-        $query_pewangi = "SELECT MONTH(order_date) AS month, SUM(total_cost) AS total_cost
-                          FROM laundry_orders
-                          GROUP BY MONTH(order_date);";
-        $hasil = mysqli_query($koneksi, $query_pewangi);
+        <div class="container">
+            <canvas id="tiga"></canvas>
+        </div>
+    </div>
 
-        // Data arrays
-        $bulan = array();
-        $stock_quantities = array();
-        $nama_bulan = [
-            1 => 'Januari',
-            2 => 'Februari',
-            3 => 'Maret',
-            4 => 'April',
-            5 => 'Mei',
-            6 => 'Juni',
-            7 => 'Juli',
-            8 => 'Agustus',
-            9 => 'September',
-            10 => 'Oktober',
-            11 => 'November',
-            12 => 'Desember'
-        ];
 
-        // Fetch data and populate arrays
-        while ($row = mysqli_fetch_assoc($hasil)) {
-            $bulan[] = $nama_bulan[$row['month']];
-            $stock_quantities[] = $row['total_cost'];
-        }
-        ?>
-        <script>
-            // Retrieve data from PHP
-            var bulan = <?php echo json_encode($bulan); ?>;
-            var stockQuantities = <?php echo json_encode($stock_quantities); ?>;
+    
+    <?php
+    include '../koneksi.php'; 
+    $query = "SELECT YEAR(order_date) AS year, MONTH(order_date) AS month, SUM(total_cost) AS total_cost
+    FROM laundry_orders
+    GROUP BY YEAR(order_date), MONTH(order_date);
+    ";
 
-            var colors = ['rgba(255, 159, 64, 1)', 'rgba(128, 128, 128, 1)', 'rgba(255, 0, 0, 1)', 'rgba(0, 255, 0, 1)', 'rgba(0, 0, 255, 1)', 'rgba(255, 255, 0, 1)'];
+    $result = mysqli_query($koneksi, $query);
 
-            // Create chart
-            var ctx = document.getElementById('tiga').getContext('2d');
-            var chart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: bulan, // Use the month names as labels
-                    datasets: [{
-                        label: 'Pendapatan',
-                        data: stockQuantities,
-                        backgroundColor: colors,
-                        borderColor: 'rgba(0, 123, 255, 1)',
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
+    // Array untuk menyimpan data tanggal dan jumlah order
+    $dates = array();
+    $orderCounts = array();
+
+    // Mengisi array dengan data dari database
+    while ($row = mysqli_fetch_assoc($result)) {
+        $dates[] = $row['year'] . '-' . $row['month'];
+        $orderCounts[] = $row['total_cost'];
+    }
+    // Mock data
+    
+
+    ?>
+
+    <script>
+        // Retrieve data from PHP
+        var months = <?php echo json_encode($dates); ?>;
+        var orderCounts = <?php echo json_encode($orderCounts); ?>;
+
+        var colors = generateRandomColors(months.length);
+        
+        // Create chart
+        var ctx = document.getElementById('tiga').getContext('2d');
+        var chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: months,
+                datasets: [{
+                    label: 'Total Cost',
+                    data: orderCounts,
+                    fill: false,
+                    backgroundColor: colors,
+                    borderColor: 'rgba(0, 123, 255, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    x: {
+                        type: 'category',
+                        labels: months,
+                        offset: true,
+                        grid: {
+                            display: false
                         }
+                    },
+                    y: {
+                        beginAtZero: true,
+                        stepSize: 50000
                     }
                 }
-            });
-        </script>
-    </div>
+            }
+        });
+
+        function generateRandomColors(count) {
+            var colors = [];
+            for (var i = 0; i < count; i++) {
+                var color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+                colors.push(color);
+            }
+            return colors;
+        }
+    </script>
 </body>
 </html>
